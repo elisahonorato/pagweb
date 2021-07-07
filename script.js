b5fd845aed9e9137a92d2fdb5949ba57cca06b77
elisahonorato <69485908+elisahonorato@users.noreply.github.com>
2021-07-07 18:04:36 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -44,9 +44,6 @@ Papa.parse('https://raw.githubusercontent.com/elisahonorato/pagweb/main/pag%20we
         myProjects.forEach(({ image, number }) => {
             products.innerHTML += `
                 <a href="proyecto.html?nro=${number}">
-                    <div class="itemBox">
-                        <img src="${image}" >
-                    </div>
                 </a>`
         })
     }
