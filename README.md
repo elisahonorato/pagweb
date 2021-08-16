f5e919f4aabc1fec4dc622d8da07615d699fa872
Elisa Honorato <elisahonorato@Elisas-MacBook-Pro.local>
2021-08-16 14:59:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pagweb
+www.elisahonoratolira.cl
---
+++ script.js
@@ -0,0 +1,66 @@
+function transition(event)   {
+    document.getElementsByTagName("body")[0].style.background = "#000000";
+
+}
+
+function nada() {
+    document.getElementsByTagName("body")[0].style.background = "#fff";
+}
+
+Papa.parse('https://raw.githubusercontent.com/elisahonorato/pagweb/main/pag%20web.xlsx%20-%20Hoja%201.csv', {
+    download: true,
+    header: true,
+    dynamicTyping: true,
+    complete: (respuesta) => {
+        const myProjects = respuesta.data;
+        var products = $('#projects')[0]
+        myProjects.forEach(({pic_lg, type, number }) => {
+            products.innerHTML += `
+                <a href="proyecto.html?nro=${number}">
+                    <div class="itemBox" data-item="${type}">
+                        <img src="${pic_lg}" alt=${type}">
+                    </div>
+                </a>`
+        })
+        $('div.itemBox').mouseover(transition);
+        $('div.itemBox').mouseleave(nada);
+    }
+})
+
+let list = document.querySelectorAll('.list');
+
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener('click', function() {
+        let itemBox = document.querySelectorAll('.itemBox');
+        for (let j = 0; j < list.length; j++) {
+            list[j].classList.remove('active');
+        }
+        this.classList.add('active');
+
+        let dataFilter = this.getAttribute('data-Filter');
+        for (let k = 0; k < itemBox.length; k++) {
+            itemBox[k].classList.remove('active');
+            itemBox[k].classList.add('hide');
+            if (itemBox[k].getAttribute('data-item') == dataFilter || dataFilter == "all") {
+                itemBox[k].classList.remove('hide');
+                itemBox[k].classList.add('active');
+            }
+        }
+    })
+}
+
+
+//El scroll
+var secundario = document.getElementById("secundario").offsetHeight;
+window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+    if (scroll > secundario + 10) {
+        document.getElementById("principal").classList.add("fixed-top");
+    } else {
+        document.getElementById("principal").classList.remove("fixed-top");
+    }
+});
+
+
+
+
